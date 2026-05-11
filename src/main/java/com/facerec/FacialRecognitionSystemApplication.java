c678428565d949c573f8e5b9a25a762212b2f5fd
mvinduwara <mvinduwara@gmail.com>
2026-05-11 11:32:47 +0530

    Update FacialRecognitionSystemApplication.java

--- src/main/java/com/facerec/FacialRecognitionSystemApplication.java
+++ src/main/java/com/facerec/FacialRecognitionSystemApplication.java
@@ -2,12 +2,17 @@ package com.facerec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableKafka
+@EnableAsync
+@EnableScheduling
 public class FacialRecognitionSystemApplication {
-
     public static void main(String[] args) {
+        nu.pattern.OpenCV.loadLocally();
         SpringApplication.run(FacialRecognitionSystemApplication.class, args);
     }
-
 }
