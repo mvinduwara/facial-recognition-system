90afff88f7f9fb12d97faf6c7c5c0bea21f34ed7
mvinduwara <mvinduwara@gmail.com>
2026-05-11 14:29:34 +0530

    Update FeatureExtractor.java

--- src/main/java/com/facerec/recognition/FeatureExtractor.java
+++ src/main/java/com/facerec/recognition/FeatureExtractor.java
@@ -1,33 +1,40 @@
 package com.facerec.recognition;
 
 import lombok.extern.slf4j.Slf4j;
-import org.deeplearning4j.nn.graph.ComputationGraph;
-import org.deeplearning4j.util.ModelSerializer;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.ops.transforms.Transforms;
+import org.nd4j.onnxruntime.runner.OnnxRuntimeRunner;
 import org.opencv.core.Mat;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Component
 public class FeatureExtractor {
 
-    private ComputationGraph faceNetModel;
+    private OnnxRuntimeRunner onnxRuntimeRunner;
     private static final int FACE_SIZE = 160;
     private static final int EMBEDDING_SIZE = 128;
 
     @PostConstruct
     public void loadModel() throws Exception {
-        try (InputStream is = getClass().getClassLoader().getResourceAsStream("models/facenet_model.zip")) {
-            faceNetModel = ModelSerializer.restoreComputationGraph(is, true);
-            log.info("FaceNet model loaded. Input: {}x{}, Output: {} dims", FACE_SIZE, FACE_SIZE, EMBEDDING_SIZE);
+        String modelPath = getClass().getClassLoader().getResource("models/facenet.onnx").getPath();
+
+        if (System.getProperty("os.name").toLowerCase().contains("win") && modelPath.startsWith("/")) {
+            modelPath = modelPath.substring(1);
         }
+
+        onnxRuntimeRunner = OnnxRuntimeRunner.builder()
+                .modelUri(modelPath)
+                .build();
+
+        log.info("FaceNet ONNX model loaded. Input: {}x{}, Output: {} dims", FACE_SIZE, FACE_SIZE, EMBEDDING_SIZE);
     }
 
     public float[] extract(Mat faceROI) {
@@ -39,8 +46,11 @@ public class FeatureExtractor {
 
         input = input.div(127.5f).sub(1.0f);
 
-        INDArray[] outputs = faceNetModel.output(input);
-        INDArray embedding = outputs[0];
+        Map<String, INDArray> inputs = new HashMap<>();
+        inputs.put("input", input);
+
+        Map<String, INDArray> results = onnxRuntimeRunner.exec(inputs);
+        INDArray embedding = results.get("output");
 
         embedding = Transforms.normalizeVector(embedding);
         return embedding.toFloatVector();
