2060b75d00376965473731f05a1cc03de00ff1a0
mvinduwara <mvinduwara@gmail.com>
2026-05-11 11:32:58 +0530

    Create FeatureExtractor.java

---
+++ src/main/java/com/facerec/recognition/FeatureExtractor.java
@@ -0,0 +1,7 @@
+package com.facerec.recognition;
+
+import lombok.extern.slf4j.Slf4j;
+import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.deeplearning4j.util.ModelSerializer;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory
