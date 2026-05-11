7671d12845d9c3e80e6bbb0f460e66138d411d33
mvinduwara <mvinduwara@gmail.com>
2026-05-11 11:17:35 +0530

    first commit

---
+++ src/main/java/com/facerec/FacialRecognitionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.facerec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacialRecognitionSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacialRecognitionSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/facerec/FacialRecognitionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facerec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacialRecognitionSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
